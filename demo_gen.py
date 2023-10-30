7343f2341e1e717bdb6a3e5a8bd6c3f57fb92749
Bohan Hou <bohanhou@bohans-mbp.acasmart.jh.edu>
2023-10-30 15:07:14 -0400

    demo_gen_prompt

--- demo_gen.py
+++ demo_gen.py
@@ -8,7 +8,22 @@ tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 state = 'md'
 
 messages = [
-    {"role": "user", "content": f"Generate a demographic profile of someone from {state} that feels hesitant about COVID vaccination"},
+    {"role": "user",
+     "content":
+        f'''Generate a demographic profile of someone from 
+            {state} that feels hesitant about COVID vaccination.
+            
+            Example: 
+                - gender: male
+                - race: Hispanic
+                - age: 45 years old
+                - occupation: farm owner
+                - religion: atheist
+                - political belief: neutral
+            
+            Generate profile:
+        '''
+     },
 #    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
 #    {"role": "user", "content": "Do you have mayonnaise recipes?"}
 ]
@@ -17,6 +32,6 @@ encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 
 model_inputs = encodeds.to(device)
 
-generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+generated_ids = model.generate(model_inputs, max_new_tokens=3000, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
 print(decoded[0])
