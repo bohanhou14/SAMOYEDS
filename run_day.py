1f0c4a86a695702d55d6677c9b5462a7fb92d953
Bohan Hou <bohanhou@bohans-mbp.acasmart.jh.edu>
2023-10-31 16:50:29 -0400

    small_bug

--- run_day.py
+++ run_day.py
@@ -3,7 +3,7 @@ from agent import Agent
 from engine import Engine
 from utils import read_profile
 
-with open("profiles-agent_num=10-top_p=0.7-temp=2.0.pkl", "rb") as f:
+with open("profiles/profiles-agent_num=10-top_p=0.7-temp=2.0.pkl", "rb") as f:
     # a list of dictionaries
     profiles = pickle.load(f)
 
