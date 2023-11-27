72c5abd74ac75a2e60f7c9e0c4bcb2fce5bf4080
Bohan Hou <bohanhou@bohans-mbp.acasmart.jh.edu>
2023-11-27 11:33:20 -0500

    update

--- mii_inference.py
+++ mii_inference.py
@@ -1,4 +1,6 @@
 from mii import pipeline
 pipe = pipeline("mistralai/Mistral-7B-v0.1")
 output = pipe(["Hello, my name is", "DeepSpeed is"], max_new_tokens=128)
-print(type(output))
+
+print(output[0])
+print(output[1])
