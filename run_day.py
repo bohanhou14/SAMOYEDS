7a0274ec02167d379209bd035a2db2178741f209
Bohan Hou <bohanhou@Bohans-MacBook-Pro.local>
2023-11-28 22:13:48 -0500

    run_day

--- run_day.py
+++ run_day.py
@@ -20,18 +20,14 @@ agent_2 = Agent(profile=profile_2)
 profile_str_2 = agent_0.get_profile_str()
 
 
-messages_0 = f'''
-<s>[INST] Pretend you are a person with following characteristics: \n{profile_str_0}\n [/INST]
-What is your attitude towards getting COVID vaccination? 
-Choose from definitely yes, probably yes, probably no, definitely no. </s> 
+messages_0 = f'''<s>[INST]Pretend you are a person with following characteristics: \n{profile_str_0}\n[/INST]
+What is your attitude towards getting COVID vaccination? Choose from definitely yes, probably yes, probably no, definitely no.</s> 
 '''
 
 
 
-messages_1 = f'''
-<s>[INST] Pretend you are a person with following characteristics: \n{profile_str_1}\n [/INST]
-What is your attitude towards getting COVID vaccination? 
-Choose from definitely yes, probably yes, probably no, definitely no. </s> 
+messages_1 = f'''<s>[INST] Pretend you are a person with following characteristics: \n{profile_str_1}\n [/INST]
+What is your attitude towards getting COVID vaccination? Choose from definitely yes, probably yes, probably no, definitely no. </s> 
 '''
 
 messages_2 = f'''
