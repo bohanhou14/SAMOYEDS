7933d464d7ff0d4fd9c2525fd7bf1cbd775c9cdf
Bohan Hou <bohanhou@Bohans-MacBook-Pro.local>
2023-12-05 10:09:18 -0500

    argparse

--- demo_gen.py
+++ demo_gen.py
@@ -6,58 +6,70 @@ from tqdm import trange
 from engine import Engine
 import pandas as pd
 import pickle
+import argparse
 
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-state = 'United States'
-attitude = '"probably no or definitely no"'
 
-messages = [
-    {"role": "user",
-     "content":
-        f'''Generate a demographic profile of someone from 
-            {state} that will say {attitude} to COVID vaccination.
-            
-            Example: 
-                - Name: Garcia Marquez
-                - Gender: male
-                - Race: Hispanic
-                - Education: High School
-                - Age: 45 years old
-                - Occupation: farm owner
-                - Religion: atheist
-                - Political belief: neutral
-            
-            Generate profile:
-        '''
-     },
-#    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-#    {"role": "user", "content": "Do you have mayonnaise recipes?"}
-]
-model_inputs = tokenizer.apply_chat_template(messages, tokenize = False)
-p = 0.7
-temp = 1.5
-model = LLM("mistralai/Mistral-7B-Instruct-v0.1", tensor_parallel_size=1)
-sampling_params = SamplingParams(
-    top_p = p,
-    temperature = temp,
-    max_tokens = 512
-)
-profiles = []
-agent_num = 500
-for i in range(agent_num):
-    res = model.generate(model_inputs, sampling_params)
-    profile = parse_profile(res[0].outputs[0].text)
-    if 'Gender' not in profile.keys():
-        print(f"Extraction failed: {res[0].outputs[0].text}")
-        continue
-    profiles.append(profile)
 
-save_name = f"profiles/profiles-state-{state}-attitude-{attitude}-agent_num={agent_num}-top_p={p}-temp={temp}"
-with open(f'{save_name}.pkl', 'wb') as f:
-    pickle.dump(profiles, f)
-df = pd.DataFrame(profiles)
-df.to_csv(f'{save_name}.tsv', sep='\t')
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_agents", type=int)
+    parser.add_argument("p", type=float)
+    parser.add_argument("temp", type=float)
+    parser.add_argument("--state",default="United States")
+    parser.add_argument("--attitude", default='"probably no or definitely no"')
+    return parser.parse_args()
 
+if __name__=='__main__':
+    args = parse_args()
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+    state = args.state
+    attitude = '"probably no or definitely no"'
+    messages = [
+        {"role": "user",
+         "content":
+             f'''Generate a demographic profile of someone from 
+                {state} that will say {attitude} to COVID vaccination.
+
+                Example: 
+                    - Name: Garcia Marquez
+                    - Gender: male
+                    - Race: Hispanic
+                    - Education: High School
+                    - Age: 45 years old
+                    - Occupation: farm owner
+                    - Religion: atheist
+                    - Political belief: neutral
+
+                Generate profile:
+            '''
+         },
+        #    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+        #    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+    ]
+    model_inputs = tokenizer.apply_chat_template(messages, tokenize=False)
+    p = args.p
+    temp = args.temp
+    model = LLM("mistralai/Mistral-7B-Instruct-v0.1", tensor_parallel_size=1)
+    sampling_params = SamplingParams(
+        top_p=p,
+        temperature=temp,
+        max_tokens=512
+    )
+    profiles = []
+    agent_num = args.num_agents
+    for i in trange(agent_num):
+        res = model.generate(model_inputs, sampling_params)
+        profile = parse_profile(res[0].outputs[0].text)
+        if 'Gender' not in profile.keys():
+            print(f"Extraction failed: {res[0].outputs[0].text}")
+            continue
+        profiles.append(profile)
+
+    save_name = f"profiles/profiles-state-{state}-attitude-{attitude}-agent_num={agent_num}-top_p={p}-temp={temp}"
+    with open(f'{save_name}.pkl', 'wb') as f:
+        pickle.dump(profiles, f)
+    df = pd.DataFrame(profiles)
+    df.to_csv(f'{save_name}.tsv', sep='\t')
 
 
 # gender = [p['Gender'] for p in profiles]
