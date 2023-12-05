c0f4df8deb62f06d3af981045a19c509512a035a
Bohan Hou <bohanhou@Bohans-MacBook-Pro.local>
2023-12-05 09:02:34 -0500

    migrate

--- demo_gen.py
+++ demo_gen.py
@@ -6,7 +6,7 @@ import pandas as pd
 from engine import Engine
 import pickle
 
-tokenizer = AutoTokenizer.from_pretrained("/mnt/petrelfs/share_data/llm_data/mistral_7b/mistral-7B-v0.1/")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 state = 'Maryland'
 attitude = 'hesitant'
 
@@ -38,7 +38,7 @@ model_inputs = encodeds
 
 p = 0.7
 temp = 2.0
-model = LLM("/mnt/petrelfs/share_data/llm_data/mistral_7b/mistral-7B-v0.1/", tensor_parallel_size=1)
+model = LLM("mistralai/Mistral-7B-Instruct-v0.1", tensor_parallel_size=1)
 sampling_params = SamplingParams(
     top_p = p,
     temperature = temp,
