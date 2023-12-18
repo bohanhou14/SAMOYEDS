ffd924881345dcd2ee435df2b698acaf9a9b73de
zacdermody <zacdermody@gmail.com>
2023-12-18 13:42:28 -0500

    updated recommender system

--- recommender.py
+++ recommender.py
@@ -1,7 +1,31 @@
+from datetime import datetime
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
-class Recommender:
-    def __init__(self, model_path = "paraphrase-MiniLM-L6-v2"):
-        self.model = SentenceTransformer(model_path)
-    def recommend(self, tweets):
-        return ""
+class TweetRecommender:
+    def __init__(self, model_name='paraphrase-MiniLM-L6-v2'):
+        self.model = SentenceTransformer(model_name)
+
+    def encode_items(self, items):
+        return [self.model.encode(item) for item in items]
+
+    def calculate_scores(self, profile_embedding, tweet_embeddings):
+        similarities = cosine_similarity([profile_embedding], tweet_embeddings)[0]
+        return similarities
+
+    def recommend(self, tweets, profiles, num_recommendations=10):
+        tweet_embeddings = self.encode_items(tweets)
+        all_recommendations = []
+
+        for profile in profiles:
+            profile_embedding = self.model.encode(profile)
+            scores = self.calculate_scores(profile_embedding, tweet_embeddings)
+
+            top_tweet_indices = scores.argsort()[::-1][:num_recommendations]
+            top_tweets = [tweets[i] for i in top_tweet_indices]
+
+            all_recommendations.append(top_tweets)
+
+        return all_recommendations
+
