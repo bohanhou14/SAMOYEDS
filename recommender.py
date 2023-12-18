c24b647396353332c752a01bd8ef119cc70f3bc3
zacdermody <zacdermody@gmail.com>
2023-12-18 14:08:39 -0500

    update

--- recommender.py
+++ recommender.py
@@ -2,35 +2,32 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
-class Recommender:
+class TweetRecommender:
     def __init__(self, model_name='paraphrase-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
 
     def encode_items(self, items):
-        return [self.model.encode(item) for item in items]
+        return [self.model.encode(item.text if hasattr(item, 'text') else item) for item in items]
 
     def calculate_scores(self, profile_embedding, tweet_embeddings):
         similarities = cosine_similarity([profile_embedding], tweet_embeddings)[0]
         return similarities
 
-    def recommend(self, tweets, agents, num_recommendations=10):
-        tweet_embeddings = self.encode_items(tweets.text)
+    def recommend(self, tweet_objects, profiles, num_recommendations=10):
+        # Encoding only the text attribute of tweets
+        tweet_embeddings = self.encode_items(tweet_objects)
         all_recommendations = []
 
-        for agent in agents:
-            agent_tweets = agent.tweets  # Accessing agent's own tweets
-            profile_embedding = self.model.encode(agent.get_profile_str())
+        for profile in profiles:
+            profile_embedding = self.model.encode(profile)
             scores = self.calculate_scores(profile_embedding, tweet_embeddings)
 
-            sorted_indices = scores.argsort()[::-1]
-            top_tweets = []
-            for i in sorted_indices:
-                if len(top_tweets) < num_recommendations:
-                    if tweets[i] not in agent_tweets:
-                        top_tweets.append(tweets[i])
-                else:
-                    break
+            sorted_indices = scores.argsort()[::-1][:num_recommendations]
+            top_tweet_objects = [tweet_objects[i] for i in sorted_indices]
 
-            all_recommendations.append(top_tweets)
+            # Extracting text from tweet objects for the recommendation list
+            top_tweets_texts = [tweet.text for tweet in top_tweet_objects]
+            all_recommendations.append(top_tweets_texts)
 
         return all_recommendations
+
