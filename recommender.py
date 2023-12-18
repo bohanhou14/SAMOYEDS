e83fcd15844cec628993da158ad194282f185ba6
zacdermody <zacdermody@gmail.com>
2023-12-18 13:55:22 -0500

    update

--- recommender.py
+++ recommender.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
-class TweetRecommender:
+class Recommender:
     def __init__(self, model_name='paraphrase-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
 
