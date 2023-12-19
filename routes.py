db6f553db6844d7d1eb8126aa970a5c54269e3b4
Vijay Murari Tiyyala <vijaymurari.tiyyala@gmail.com>
2023-12-18 20:32:27 -0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+import os
+import json
 
 app = Flask(__name__)
 
@@ -6,9 +8,22 @@ app = Flask(__name__)
 simulation_data = {"value": 0}
 simulation_status = {"status": "stopped"}
 
-@app.route('/simulation-status', methods=['GET'])
-def get_simulation_status():
-    return jsonify(simulation_status)
+@app.route('/upload-data', methods=['POST'])
+def upload_data():
+    # Specify the directory where you want to save the data
+    save_directory = '/home/vtiyyal1/projectml/'
+    os.makedirs(save_directory, exist_ok=True)
+
+    # Extract data from the request
+    data = request.json
+    filename = data.get('filename', 'default.json')  # You can pass a filename in your request
+
+    # Save data to a file in the specified directory
+    file_path = os.path.join(save_directory, filename)
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+    return jsonify({"message": "Data uploaded successfully"}), 200
 
 @app.route('/simulation-start', methods=['POST'])
 def start_simulation():
@@ -23,6 +38,37 @@ def start_simulation():
     # You now have user_demographics, tweet_data, news_data, and policies to use in your simulation
 
     return jsonify({"message": "Simulation started with provided data"}), 200
+
+
+@app.route('/get-json-file', methods=['GET'])
+def get_json_file():
+    file_directory = '/mnt/hwfile/chenzhi/ABE/SAMOYEDS/'#'/home/vtiyyal1/projectml'
+    filename = 'vj_json.json'
+
+    file_path = os.path.join(file_directory, filename)
+    if os.path.exists(file_path):
+        return send_from_directory(file_directory, filename)
+    else:
+        return jsonify({"error": "File not found"}), 404
+    
+"""
+@app.route('/get-pkl-file', methods=['GET'])
+def get_pkl_file():
+    file_directory = '/home/vtiyyal1/projectml'
+    filename = 'combined_posts_texts_covid.pkl'
+
+    file_path = os.path.join(file_directory, filename)
+    if os.path.exists(file_path):
+        return send_from_directory(file_directory, filename)
+    else:
+        return jsonify({"error": "File not found"}), 404
+"""
+
+@app.route('/simulation-status', methods=['GET'])
+def get_simulation_status():
+    return jsonify(simulation_status)
+
+
 
 @app.route('/data', methods=['GET'])
 def get_data():
