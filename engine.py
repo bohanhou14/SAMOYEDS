bb8345b5dc3ebfa74be67b9cad50fa9808c315fb
Abe <13381498805@163.com>
2024-03-14 17:02:22 -0400

    qwen_gen

--- engine.py
+++ engine.py
@@ -67,6 +67,7 @@ class Engine:
             return self.tokenizer.apply_chat_template(msg, tokenize=False)
         
         def convert_qwen(msg):
+            print(msg)
             text = self.tokenizer.apply_chat_template(msg, tokenize=False, add_generation_prompt=True)
             return self.tokenizer([text], return_tensors="pt").to(self.device)
         if messages_list == None:
