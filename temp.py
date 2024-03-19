4cfecb53fa1776065c758379a52d9f0f3b0d3589
Abe <13381498805@163.com>
2024-03-19 15:30:29 -0400

    qwen_gen_param_change

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 from engine import Engine
 import pickle
 from agent import Agent
-with open("profiles/profiles-state-United States=attitude=probably_no_or_definitely_no-agent_num=10-top_p=0.7-temp=1.5.pkl", "rb") as f:
+with open("profiles/profiles-agent_num=10-top_p=0.7-temp=2.0.pkl", "rb") as f:
     # a list of dictionaries
     profiles = list(pickle.load(f))
 
