117f509e283a17b4dde27b280677bae4b0178a0c
Abe <13381498805@163.com>
2024-03-19 16:34:30 -0400

    prompt

--- prompts.py
+++ prompts.py
@@ -24,7 +24,8 @@ def profile_prompt(profile_str):
                 - Occupation:  small business owner 
                 - Political belief:  moderate democrat 
                 - Religion:  Buddhist. 
-                - Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no]: probably yes.
+                Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no] in one-word: 
+                    probably yes.
 
                 Example B:
                     - Gender:  female
@@ -33,11 +34,13 @@ def profile_prompt(profile_str):
                     - Occupation:  stay-at-home mom
                     - Political belief:  Republican
                     - Religion:  Baptist
-                    - Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no]: probably no.
+                    Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no] in one-word: 
+                        probably no.
 
                 Question:
                     {profile_str}
-                    - Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no]: 
+                    Attitude towards COVID vaccination from [definitely yes, probably yes, probably no, definitely no] in one-word:
+                         
                 '''
              }]
 
